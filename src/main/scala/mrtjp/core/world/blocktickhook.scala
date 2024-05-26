282875e98a742a5a86f8a140927198417e35e586
Jason Mitchell <mitchej@gmail.com>
2024-05-25 19:54:09 -0700

    Now with less cascade loading

--- src/main/scala/mrtjp/core/world/blocktickhook.scala
+++ src/main/scala/mrtjp/core/world/blocktickhook.scala
@@ -63,42 +63,45 @@ object BlockUpdateHandler {
 
     // Reproduces same algorithm used for random block updates
     val world = event.world.asInstanceOf[WorldServer]
-
+    val provider = world.getChunkProvider
     val cIt = getActiveChunkSet(world).iterator()
     while (cIt.hasNext) {
       val chunkPos = cIt.next()
-      val chunk =
-        world.getChunkFromChunkCoords(chunkPos.chunkXPos, chunkPos.chunkZPos)
-      val ebstorage = chunk.getBlockStorageArray
+      if (provider.chunkExists(chunkPos.chunkXPos, chunkPos.chunkZPos)) {
 
-      var k = 0
-      while (k < ebstorage.length) {
-        val ebs = ebstorage(k)
-        if (ebs != null) {
-          var i = 0
-          while (i < 3) {
-            updateLCG = updateLCG * 3 + 1013904223
-            val i2 = updateLCG >> 2
-            val j2 = i2 & 15
-            val k2 = i2 >> 8 & 15
-            val l2 = i2 >> 16 & 15
-            val block = ebs.getBlockByExtId(j2, l2, k2)
+        val chunk =
+          world.getChunkFromChunkCoords(chunkPos.chunkXPos, chunkPos.chunkZPos)
+        val ebstorage = chunk.getBlockStorageArray
 
-            var j = 0
-            while (j < handlers.length) {
-              handlers(j).onBlockUpdate(
-                world,
-                j2 + chunk.xPosition * 16,
-                l2 + ebs.getYLocation,
-                k2 + chunk.zPosition * 16,
-                block
-              )
-              j += 1
+        var k = 0
+        while (k < ebstorage.length) {
+          val ebs = ebstorage(k)
+          if (ebs != null) {
+            var i = 0
+            while (i < 3) {
+              updateLCG = updateLCG * 3 + 1013904223
+              val i2 = updateLCG >> 2
+              val j2 = i2 & 15
+              val k2 = i2 >> 8 & 15
+              val l2 = i2 >> 16 & 15
+              val block = ebs.getBlockByExtId(j2, l2, k2)
+
+              var j = 0
+              while (j < handlers.length) {
+                handlers(j).onBlockUpdate(
+                  world,
+                  j2 + chunk.xPosition * 16,
+                  l2 + ebs.getYLocation,
+                  k2 + chunk.zPosition * 16,
+                  block
+                )
+                j += 1
+              }
+              i += 1
             }
-            i += 1
           }
+          k += 1
         }
-        k += 1
       }
     }
   }
